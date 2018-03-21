3d668def344ba4a5aae48580ccb0774052a29e3c
Matt Derry <matt@goodoldmatt.com>
2018-03-21 14:46:13 +0000

    Adds section completion status to model

---
+++ src/SFA.DAS.PSRService.Domain/Enums/CompletionStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.PSRService.Domain.Enums
+{
+    public enum CompletionStatus
+    {
+        Incomplete = 0,        
+        InProgress = 1,
+        Completed = 2
+    }
+}
---
+++ src/SFA.DAS.PSRService.Web/ViewModels/SectionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.PSRService.Domain.Entities;
+using SFA.DAS.PSRService.Domain.Enums;
+
+namespace SFA.DAS.PSRService.Web.ViewModels
+{
+    public class SectionViewModel: Section
+    {
+       
+    }
+}
