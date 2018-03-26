efe60cfbb2f135939e220a6f3519a5b03648a3ca
Greg Barnes <gregdbarnes@gmail.com>
2018-03-26 16:15:30 +0100

    More merge conflicts

--- src/SFA.DAS.PSRService.Web/wwwroot/js/site.js
+++ src/SFA.DAS.PSRService.Web/wwwroot/js/site.js
@@ -1,1 +1,55 @@
 ﻿// Write your JavaScript code.
+
+$(function() {
+  var formValidator,
+    $form = $('.questionForm');
+
+  // add handler to the forms submit action
+  $form.submit(function() {
+    if (!formValidator) {
+      formValidator = $form.validate({}); // Get existing jquery validate object
+    }
+
+    var errorList = [];
+
+    // get existing summary errors from jQuery validate
+    $.each(formValidator.errorList, function(index, errorListItem) {
+      errorList.push(errorListItem.message);
+    });
+
+    // add our own errors
+    if (testForErrorCondidtionA()) {
+      errorList.push('Please fix error condition A!');
+    }
+
+    if (testForErrorCondidtionB()) {
+      errorList.push('Please fix error condition B!');
+    }
+
+    // No errors, do nothing
+    if (0 === errorList.length) {
+      return true; // allow submit
+    }
+
+    // find summary div
+    var $summary = $form.find('[data-valmsg-summary=true]');
+
+    // find the unordered list
+    var $ul = $summary.find('ul');
+
+    // Clear existing errors from DOM by removing all element from the list
+    $ul.empty();
+
+    // Add all errors to the list
+    $.each(errorList, function(index, message) {
+      $('<li />')
+        .html(message)
+        .appendTo($ul);
+    });
+
+    // Add the appropriate class to the summary div
+    $summary.removeClass('validation-summary-valid').addClass('validation-summary-errors');
+
+    return false; // Block the submit
+  });
+});
