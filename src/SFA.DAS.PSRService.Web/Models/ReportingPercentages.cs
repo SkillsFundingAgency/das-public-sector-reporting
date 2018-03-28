87ac232fa111c493a6b437c5d3385033dfa32b60
desderry <matt@goodoldmatt.co.uk>
2018-03-28 10:54:24 +0000

    Adds reporting calculcations to summary (#24)

---
+++ src/SFA.DAS.PSRService.Web/Models/ReportingPercentages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.PSRService.Domain.Entities;
+
+namespace SFA.DAS.PSRService.Web.ViewModels
+{
+    public class ReportingPercentages
+    {
+        public decimal EmploymentStarts { get; set; }
+
+        public decimal TotalHeadCount { get; set; }
+        public decimal NewThisPeriod { get; set; }
+       
+    }
+}
