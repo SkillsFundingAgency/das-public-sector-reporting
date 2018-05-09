dc48142c4928faba0a9617748e044f88ed0d6c98
Neil Winstanley <Winsto@users.noreply.github.com>
2018-05-09 16:42:32 +0100

    Remove redundant mock logger and tidy up usings.

--- src/SFA.DAS.PSRService.Web.UnitTests/ReportControllerTests/ReportControllerTestBase.cs
+++ src/SFA.DAS.PSRService.Web.UnitTests/ReportControllerTests/ReportControllerTestBase.cs
@@ -6,11 +6,9 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.PSRService.Domain.Entities;
-using SFA.DAS.PSRService.Domain.Enums;
 using SFA.DAS.PSRService.Web.Controllers;
 using SFA.DAS.PSRService.Web.Models;
 using SFA.DAS.PSRService.Web.Services;
-using SFA.DAS.PSRService.Web.ViewModels;
 
 namespace SFA.DAS.PSRService.Web.UnitTests.ReportControllerTests
 {
@@ -23,7 +21,6 @@ namespace SFA.DAS.PSRService.Web.UnitTests.ReportControllerTests
         private Mock<IEmployerAccountService> _employeeAccountServiceMock;
         public Mock<IUserService> _userServiceMock;
         private Mock<IPeriodService> _periodServiceMock;
-        private Mock<ILogger<ReportController>> _mockLogging;
         protected IList<Report> _reportList = ReportTestModelBuilder.ReportsWithValidSections();
         private EmployerIdentifier _employerIdentifier;
 
@@ -32,7 +29,7 @@ namespace SFA.DAS.PSRService.Web.UnitTests.ReportControllerTests
         {
             _mockUrlHelper = new Mock<IUrlHelper>(MockBehavior.Strict);
             _mockReportService = new Mock<IReportService>(MockBehavior.Strict);
-            _mockLogging = new Mock<ILogger<ReportController>>(MockBehavior.Strict);
+            new Mock<ILogger<ReportController>>(MockBehavior.Strict);
             _employeeAccountServiceMock = new Mock<IEmployerAccountService>(MockBehavior.Strict);
             _userServiceMock = new Mock<IUserService>(MockBehavior.Strict);
             _periodServiceMock = new Mock<IPeriodService>(MockBehavior.Strict);
@@ -40,7 +37,7 @@ namespace SFA.DAS.PSRService.Web.UnitTests.ReportControllerTests
 
             _periodServiceMock.Setup(s => s.GetCurrentPeriod()).Returns(new Period(DateTime.UtcNow));
 
-            _controller = new ReportController(_mockLogging.Object, _mockReportService.Object, _employeeAccountServiceMock.Object, _userServiceMock.Object, null, _periodServiceMock.Object) { Url = _mockUrlHelper.Object };
+            _controller = new ReportController(_mockReportService.Object, _employeeAccountServiceMock.Object, _userServiceMock.Object, null, _periodServiceMock.Object) { Url = _mockUrlHelper.Object };
             _employerIdentifier = new EmployerIdentifier() { AccountId = "ABCDE", EmployerName = "EmployerName" };
 
             _employeeAccountServiceMock.Setup(s => s.GetCurrentEmployerAccountId(It.IsAny<HttpContext>()))
