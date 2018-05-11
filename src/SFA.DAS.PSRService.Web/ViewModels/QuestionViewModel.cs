42ff15ea0e42f6e587606e10fc59064f096c1d7c
Neil Winstanley <Winsto@users.noreply.github.com>
2018-05-11 15:29:54 +0100

    Quick bit of tidy up, whilst I've spotted it.

--- src/SFA.DAS.PSRService.Web/ViewModels/QuestionViewModel.cs
+++ src/SFA.DAS.PSRService.Web/ViewModels/QuestionViewModel.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using SFA.DAS.PSRService.Domain.Entities;
-using SFA.DAS.PSRService.Domain.Enums;
+﻿using SFA.DAS.PSRService.Domain.Enums;
 using SFA.DAS.PSRService.Web.Attributes;
 
 namespace SFA.DAS.PSRService.Web.ViewModels
@@ -13,13 +6,9 @@ namespace SFA.DAS.PSRService.Web.ViewModels
     public class QuestionViewModel
     {
         public string Id { get; set; }
-       [CustomAnswerValidation("Type")]
+        [CustomAnswerValidation("Type")]
         public string Answer { get; set; }
         public bool Optional { get; set; }
         public QuestionType Type { get; set; }
-
-        //public ModelStateDictionary ModelStateErrors { get; set; }
     }
-
-    
 }
