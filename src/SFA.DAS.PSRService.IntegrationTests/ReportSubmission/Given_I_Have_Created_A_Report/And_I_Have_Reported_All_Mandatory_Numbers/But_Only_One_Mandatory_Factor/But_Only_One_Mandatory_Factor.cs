0f08066e0c6d63b72d921ca9d6da09be5baa8531
Neil Winstanley <Winsto@users.noreply.github.com>
2018-05-14 16:04:38 +0100

    Should have renamed namespace when I renamed the class.

--- src/SFA.DAS.PSRService.IntegrationTests/ReportSubmission/Given_I_Have_Created_A_Report/And_I_Have_Reported_All_Mandatory_Numbers/But_Only_One_Mandatory_Factor/But_Only_One_Mandatory_Factor.cs
+++ src/SFA.DAS.PSRService.IntegrationTests/ReportSubmission/Given_I_Have_Created_A_Report/And_I_Have_Reported_All_Mandatory_Numbers/But_Only_One_Mandatory_Factor/But_Only_One_Mandatory_Factor.cs
@@ -1,4 +1,4 @@
-﻿namespace SFA.DAS.PSRService.IntegrationTests.ReportSubmission.Given_I_Have_Created_A_Report.And_I_Have_Reported_All_Mandatory_Numbers.But_Only_Some_Mandatory_Factors
+﻿namespace SFA.DAS.PSRService.IntegrationTests.ReportSubmission.Given_I_Have_Created_A_Report.And_I_Have_Reported_All_Mandatory_Numbers.But_Only_One_Mandatory_Factor
 {
     public abstract class But_Only_One_Mandatory_Factor
     :And_I_Have_Reported_All_Mandatory_Numbers
--- src/SFA.DAS.PSRService.IntegrationTests/ReportSubmission/Given_I_Have_Created_A_Report/And_I_Have_Reported_All_Mandatory_Numbers/But_Only_One_Mandatory_Factor/When_I_Submit_The_Report.cs
+++ src/SFA.DAS.PSRService.IntegrationTests/ReportSubmission/Given_I_Have_Created_A_Report/And_I_Have_Reported_All_Mandatory_Numbers/But_Only_One_Mandatory_Factor/When_I_Submit_The_Report.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc.Routing;
 using Moq;
 using NUnit.Framework;
 
-namespace SFA.DAS.PSRService.IntegrationTests.ReportSubmission.Given_I_Have_Created_A_Report.And_I_Have_Reported_All_Mandatory_Numbers.But_Only_Some_Mandatory_Factors
+namespace SFA.DAS.PSRService.IntegrationTests.ReportSubmission.Given_I_Have_Created_A_Report.And_I_Have_Reported_All_Mandatory_Numbers.But_Only_One_Mandatory_Factor
 {
     public sealed class When_I_Submit_The_Report
     : But_Only_One_Mandatory_Factor
