b48c010d48828a5449567810d91447999e60d62e
Neil Winstanley <Winsto@users.noreply.github.com>
2018-05-14 14:34:41 +0100

    Name now better reflects the actual setup.

--- src/SFA.DAS.PSRService.IntegrationTests/ReportSubmission/Given_I_Have_Created_A_Report/And_I_Have_Reported_All_Mandatory_Numbers/But_Only_Some_Mandatory_Factors/When_I_Submit_The_Report.cs
+++ src/SFA.DAS.PSRService.IntegrationTests/ReportSubmission/Given_I_Have_Created_A_Report/And_I_Have_Reported_All_Mandatory_Numbers/But_Only_Some_Mandatory_Factors/When_I_Submit_The_Report.cs
@@ -3,7 +3,7 @@
 namespace SFA.DAS.PSRService.IntegrationTests.ReportSubmission.Given_I_Have_Created_A_Report.And_I_Have_Reported_All_Mandatory_Numbers.But_Only_Some_Mandatory_Factors
 {
     public sealed class When_I_Submit_The_Report
-    : But_Only_Some_Mandatory_Factors
+    : But_Only_One_Mandatory_Factor
     {
         [Test]
         public void Then_Fails()
