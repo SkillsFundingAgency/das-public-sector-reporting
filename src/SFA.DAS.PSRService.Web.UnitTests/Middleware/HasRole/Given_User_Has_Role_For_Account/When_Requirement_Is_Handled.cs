8dbda1e7c5c06d2e65d2ec2d4f4ae1eb7dacc94b
Neil Winstanley <Winsto@users.noreply.github.com>
2018-05-18 14:33:26 +0100

    Oops, need to inherit from correct base.

--- src/SFA.DAS.PSRService.Web.UnitTests/Middleware/HasRole/Given_User_Has_Role_For_Account/When_Requirement_Is_Handled.cs
+++ src/SFA.DAS.PSRService.Web.UnitTests/Middleware/HasRole/Given_User_Has_Role_For_Account/When_Requirement_Is_Handled.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.PSRService.Web.UnitTests.Middleware.HasRole.Given_User_Has_Rol
 {
     [ExcludeFromCodeCoverage]
     public sealed class When_Requirement_Is_Handled
-    :Given_Context_Resource_Is_Not_An_Action_Context.Given_Context_Resource_Is_Not_An_Action_Context
+    :Given_User_Has_Role_For_Account
     {
         protected override void When()
         {
