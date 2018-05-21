d98d9f651cf8a9a147e4e4dd3d9183d62b6532a2
Maxim Beliy <maxim.beliy@mobiess.com>
2018-05-21 08:57:27 +0100

    create report handler records audit window start and update timestamp

--- src/SFA.DAS.PSRService.Application.UnitTests/ReportHandlerTests/Given_I_Create_A_Report.cs
+++ src/SFA.DAS.PSRService.Application.UnitTests/ReportHandlerTests/Given_I_Create_A_Report.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using AutoMapper;
 using Microsoft.Extensions.FileProviders;
@@ -22,7 +19,6 @@ namespace SFA.DAS.PSRService.Application.UnitTests.ReportHandlerTests
         private Mock<IReportRepository> _reportRepositoryMock;
         private Mock<IFileProvider> _fileProviderMock;
         private CreateReportHandler _createReportHandler;
-        private ReportDto reportDto;
         private Report _report;
 
         [SetUp]
@@ -33,98 +29,86 @@ namespace SFA.DAS.PSRService.Application.UnitTests.ReportHandlerTests
             _fileProviderMock = new Mock<IFileProvider>();
             _createReportHandler = new CreateReportHandler(_reportRepositoryMock.Object, _mapperMock.Object, _fileProviderMock.Object);
 
-            var reportingDataTest = "";
-            reportDto = new ReportDto()
-            {
-                EmployerId = "ABCDE",
-                ReportingPeriod = "1718",
-                Id = Guid.NewGuid(),
-                Submitted = false,
-                ReportingData = reportingDataTest
-
-
-            };
-
-            _report = new Report()
+            _report = new Report
             {
                 EmployerId = "ABCDE",
                 ReportingPeriod = "1718",
                 Id = Guid.NewGuid(),
                 Submitted = false
-
-
             };
-            var fileInfo = new StringFileInfo(reportingDataTest, "QuestionConfig.json");
+            var fileInfo = new StringFileInfo("", "QuestionConfig.json");
 
-            _reportRepositoryMock.Setup(s => s.Create(It.IsAny<ReportDto>()));
             _fileProviderMock.Setup(s => s.GetFileInfo(It.IsAny<string>())).Returns(fileInfo);
             _mapperMock.Setup(s => s.Map<Report>(It.IsAny<ReportDto>())).Returns(_report);
 
         }
+
         [Test]
         public void And_An_EmployeeId_And_Period_Is_Supplied_Then_Create_Report()
         {
             // arrange
-            var createReportRequest = new CreateReportRequest { EmployerId = "ABCDE", Period = "1718" };
+            var userId = Guid.NewGuid();
+            var userName = "Bob Shurunkle";
+            ReportDto reportDto = null;
+
+            var createReportRequest = new CreateReportRequest { EmployerId = "ABCDE", Period = "1718", UserName = userName, UserId = userId};
+            _reportRepositoryMock.Setup(s => s.Create(It.IsAny<ReportDto>())).Callback<ReportDto>(r => reportDto = r).Verifiable();
 
             // act
             var result = _createReportHandler.Handle(createReportRequest, new CancellationToken()).Result;
 
+            // assert
+            _reportRepositoryMock.VerifyAll();
             Assert.AreEqual(_report.EmployerId, result.EmployerId);
-            Assert.AreEqual(result.Submitted,_report.Submitted);
-            Assert.AreEqual(result.ReportingPeriod,_report.ReportingPeriod);
-            
+            Assert.AreEqual(_report.Submitted, result.Submitted);
+            Assert.AreEqual(_report.ReportingPeriod, result.ReportingPeriod);
+            Assert.AreEqual(_report.EmployerId, reportDto.EmployerId);
+            Assert.AreEqual(_report.ReportingPeriod, reportDto.ReportingPeriod);
+            Assert.IsTrue(reportDto.UpdatedBy.Contains(userId.ToString()));
+            Assert.IsNotNull(reportDto.AuditWindowStartUtc);
+            Assert.IsNotNull(reportDto.UpdatedUtc);
         }
 
         [Test]
         public void And_An_EmployeeId_Is_Not_Supplied_Then_Throw_Error()
         {
-            
             //arrange
-            
-            var createReportRequest = new CreateReportRequest() { EmployerId = string.Empty, Period = "1718"};
+            var createReportRequest = new CreateReportRequest { EmployerId = string.Empty, Period = "1718", UserName = "Homer"};
 
             //Act
-
             Assert.Throws<Exception>(() =>_createReportHandler.Handle(createReportRequest, new CancellationToken()));
-
-          
-
-
         }
+
+        [Test]
+        public void And_An_User_Is_Not_Supplied_Then_Throw_Error()
+        {
+            // arrange            
+            var createReportRequest = new CreateReportRequest {EmployerId = "acme inc", Period = "1718"};
+
+            // act
+            // assert
+            Assert.Throws<Exception>(() => _createReportHandler.Handle(createReportRequest, new CancellationToken()));
+        }
+
         [Test]
         public void And_Period_Is_Null_Then_Throw_Error()
         {
-
             //arrange
+            var createReportRequest = new CreateReportRequest { EmployerId = "ABCDE", Period = null, UserName = "Donald"};
 
-            var createReportRequest = new CreateReportRequest() { EmployerId = "ABCDE", Period = null };
-
-            //Act
-
+            // act
+            // assert
             Assert.Throws<Exception>(() => _createReportHandler.Handle(createReportRequest, new CancellationToken()));
-
-            //Assert
-            
-
-
         }
 
         [Test]
         public void And_Period_Is_Empty_Then_Throw_Error()
         {
-
             //arrange
-
-            var createReportRequest = new CreateReportRequest() { EmployerId = "ABCDEF", Period = "" };
+            var createReportRequest = new CreateReportRequest() { EmployerId = "ABCDEF", Period = "", UserName = "Donald" };
 
             //Act
-
             Assert.Throws<Exception>(() => _createReportHandler.Handle(createReportRequest, new CancellationToken()));
-
-
-
-
         }
     }
 }
--- src/SFA.DAS.PSRService.Application/ReportHandlers/CreateReportRequest.cs
+++ src/SFA.DAS.PSRService.Application/ReportHandlers/CreateReportRequest.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using System;
+using MediatR;
 using SFA.DAS.PSRService.Domain.Entities;
 
 namespace SFA.DAS.PSRService.Application.ReportHandlers
@@ -7,7 +8,7 @@ namespace SFA.DAS.PSRService.Application.ReportHandlers
     {
         public string EmployerId { get; set; }
         public string Period { get; set; }
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
     }
-    
-    }
-
+}
