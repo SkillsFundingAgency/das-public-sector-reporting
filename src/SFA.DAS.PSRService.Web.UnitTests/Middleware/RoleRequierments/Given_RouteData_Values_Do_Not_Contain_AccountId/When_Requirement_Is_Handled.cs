afef2ea27c0b667ac47df607b7f3cb84c96ce497
Neil Winstanley <Winsto@users.noreply.github.com>
2018-05-25 12:59:45 +0100

    Add scenarios around requirements handler.

---
+++ src/SFA.DAS.PSRService.Web.UnitTests/Middleware/RoleRequierments/Given_RouteData_Values_Do_Not_Contain_AccountId/When_Requirement_Is_Handled.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+namespace SFA.DAS.PSRService.Web.UnitTests.Middleware.RoleRequierments.Given_RouteData_Values_Do_Not_Contain_AccountId
+{
+    [ExcludeFromCodeCoverage]
+    public sealed class When_Requirement_Is_Handled
+    :Given_RouteData_Values_Do_Not_Contain_AccountId
+    {
+        protected override void When()
+        {
+            SUT
+                .HandleAsync(
+                    HandlerContext);
+        }
+
+        [Test]
+        public void Then_Requirement_Is_Not_Satisfied()
+        {
+            Assert
+                .False(
+                    HandlerContext
+                        .HasSucceeded);
+        }
+    }
+}
