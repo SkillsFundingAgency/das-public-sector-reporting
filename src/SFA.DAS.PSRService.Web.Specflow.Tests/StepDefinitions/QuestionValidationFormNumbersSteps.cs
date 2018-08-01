755359e7f84211b834236f137d142bbf3623fbd2
Mike Wild <mike.wild@digital.education.gov.uk>
2018-08-01 14:00:03 +0100

    Implement scenario for question validation with no javascript; simulates turning off javescript by removing event handlers from inputs. This is probably not the best approach but has been checked in for reference.

---
+++ src/SFA.DAS.PSRService.Web.Specflow.Tests/StepDefinitions/QuestionValidationFormNumbersSteps.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Extensions;
+using SFA.DAS.PSRService.Web.Specflow.Tests.Framework.Helpers;
+using SFA.DAS.PSRService.Web.Specflow.Tests.TestSupport;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.PSRService.Web.Specflow.Tests.StepDefinitions
+{
+    public class QuestionValidationFormNumbersSteps : BaseTest
+    {
+        private const string NoJavascriptTag = "nojavascript";
+
+        [Given(@"javascript is turned off")]
+        public void GivenJavascriptIsTurnedOff()
+        {
+            ScenarioContext.Current.Add(NoJavascriptTag, true);
+        }
+
+        [Given(@"user navigates to the Your Employee question page")]
+        public void GivenUserNavigatesToTheYourEmployeeQuestionPage()
+        {
+            pageFactory.QuestionYourEmployees.Navigate();
+        }
+
+
+        [When(@"user enters any value in this page other than a whole number in one or more fields")]
+        public void WhenUserEntersAnyValueInThisPageOtherThanAWholeNumberInOneOrMoreFields()
+        {
+            var page = pageFactory.QuestionYourEmployees;
+
+            TurnOffJavaScriptBehaviour();
+
+            page.EditAtStartValue("1x");
+            page.EditAtEndValue("2x");
+            page.EditAtNewThisPeriodValue("3");
+        }
+
+        [When(@"clicks the '(.*)' button")]
+        public void WhenClicksTheButton(string p0)
+        {
+            TurnOffJavaScriptBehaviour();
+
+            pageFactory.QuestionYourEmployees.SaveQuestionAnswers();
+        }
+
+        [Then(@"display the following message '(.*)'")]
+        public void ThenDisplayTheFollowingMessage(string p0)
+        {
+            var errorsPresent = PageInteractionHelper.IsElementPresent(By.ClassName("error-summary-heading"));
+            var errorsDisplayed = PageInteractionHelper.IsElementDisplayed(By.ClassName("error-summary-heading"));
+
+            if (!errorsPresent || !errorsDisplayed)
+            {
+                throw new Exception("Errors should be displayed");   
+            }
+        }
+
+        [Then(@"the '(.*)' question page is displayed")]
+        public void ThenTheQuestionPageIsDisplayed(string p0)
+        {
+            pageFactory.QuestionYourEmployees.Verify();
+        }
+
+        private void TurnOffJavaScriptBehaviour()
+        {
+            var js = "elems = $('.js-addcommas'); " +
+                     "elems.off('input');";
+
+            webDriver.ExecuteJavaScript(js);
+        }
+    }
+}
