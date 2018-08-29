eecd38651995011039361688022ed4ec22427a8d
Neil Winstanley <Winsto@users.noreply.github.com>
2018-08-29 18:20:07 +0100

    Make a start on a type for year.

---
+++ src/SFA.DAS.PSRService.Domain.UnitTests/TwentyFirstCenturyCommonEraYearTests/A_TwentyFirstCenturyCommonEraYear.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.PSRService.Domain.Entities;
+
+namespace SFA.DAS.PSRService.Domain.UnitTests.TwentyFirstCenturyCommonEraYearTests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class A_TwentyFirstCenturyCommonEraYear
+    {
+        [TestCase("00", 2000)]
+        [TestCase("99", 2099)]
+        [TestCase("12", 2012)]
+        [TestCase("19", 2019)]
+        public void Parses_Correctly_From_Two_Digit_Year(
+            string twoDigitYear,
+            int expectedParsedYear)
+        {
+            TwentyFirstCenturyCommonEraYear
+                .ParseTwoDigitYear(twoDigitYear)
+                .AsInt
+                .Should()
+                .Be(expectedParsedYear);
+        }
+
+        [TestCase("00")]
+        [TestCase("99")]
+        [TestCase("12")]
+        [TestCase("19")]
+        public void Has_Expected_AsTwoDigitString_Property(
+            string twoDigitYear)
+        {
+            TwentyFirstCenturyCommonEraYear
+                .ParseTwoDigitYear(twoDigitYear)
+                .AsTwoDigitString
+                .Should()
+                .Be(twoDigitYear);
+        }
+
+        [TestCase("")]
+        [TestCase("    ")]
+        [TestCase("1")]
+        [TestCase("123")]
+        [TestCase("-1")]
+        [TestCase("1a")]
+        [TestCase("a1")]
+        public void Throws_ArgumentException_When_Asked_To_Parse_Invalid_Two_Digit_Year(
+            string invalidTwoDigitYear)
+        {
+            Assert
+                .That(
+                    () => TwentyFirstCenturyCommonEraYear.ParseTwoDigitYear(invalidTwoDigitYear),
+                    Throws
+                        .Exception
+                        .TypeOf<ArgumentException>());
+        }
+    }
+}
---
+++ src/SFA.DAS.PSRService.Domain/Entities/TwentyFirstCenturyCommonEraYear.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace SFA.DAS.PSRService.Domain.Entities
+{
+    public class TwentyFirstCenturyCommonEraYear
+    {
+        private DateTime firstOfJanThisYear;
+
+        private TwentyFirstCenturyCommonEraYear(DateTime instantInYear)
+        {
+            firstOfJanThisYear
+                =
+               new DateTime(
+                   year: instantInYear.Year,
+                   month: 1,
+                   day: 1);
+        }
+        public static TwentyFirstCenturyCommonEraYear ParseTwoDigitYear(string twoDigitYear)
+        {
+            var commonEra21stCenturyYearAsFourDigitString = "20" + twoDigitYear;
+
+            DateTime parsedInstant;
+
+            if (DateTime
+                .TryParseExact(
+                    commonEra21stCenturyYearAsFourDigitString,
+                    "yyyy",
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.None,
+                    out parsedInstant))
+            {
+                return  new TwentyFirstCenturyCommonEraYear(parsedInstant);
+            }
+
+            throw new ArgumentException(
+                twoDigitYear+" is not a valid two digit year", 
+                nameof(twoDigitYear));
+        }
+
+        public int AsInt => firstOfJanThisYear.Year;
+        public string AsTwoDigitString => firstOfJanThisYear.ToString("yy");
+    }
+}
