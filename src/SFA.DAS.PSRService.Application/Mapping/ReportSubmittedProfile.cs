930b4fcefdef36dc9199a321ef19204a490c0aa8
Neil Winstanley <Winsto@users.noreply.github.com>
2018-10-31 18:19:21 +0000

    Now we have some helper code around report answers mapping to submitted message is more straight forward.

--- src/SFA.DAS.PSRService.Application/Mapping/ReportSubmittedProfile.cs
+++ src/SFA.DAS.PSRService.Application/Mapping/ReportSubmittedProfile.cs
@@ -9,7 +9,44 @@ namespace SFA.DAS.PSRService.Application.Mapping
         public ReportSubmittedProfile()
         {
             CreateMap<Report, ReportSubmitted>()
-                .ConvertUsing(report => new ReportSubmitted());
+                .ConvertUsing(report => new ReportSubmitted
+                {
+                    Id = report.Id,
+                    EmployerId = report.EmployerId,
+                    ReportingPeriod = report.Period.PeriodString,
+                    Submitter = new Submitter
+                    {
+                        Email = report.SubmittedDetails.SubmittedEmail,
+                        Name = report.SubmittedDetails.SubmittedName,
+                        UserId = report.SubmittedDetails.SubmttedBy
+                    },
+                    ReportingPercentages = new Messages.Events.ReportingPercentages
+                    {
+                        EmploymentStarts = report.ReportingPercentages.EmploymentStarts,
+                        TotalHeadCount = report.ReportingPercentages.TotalHeadCount,
+                        NewThisPeriod = report.ReportingPercentages.NewThisPeriod
+                    },
+                    Answers = new Answers
+                    {
+                        YourEmployees = new YourEmployees
+                        {
+                            AtStart = report.Answers.YourEmployees.AtStart,
+                            AtEnd = report.Answers.YourEmployees.AtEnd,
+                            NewThisPeriod = report.Answers.YourEmployees.NewThisPeriod
+                        },
+                        YourApprentices = new YourApprentices
+                        {
+                            AtStart = report.Answers.YourApprentices.AtStart,
+                            AtEnd = report.Answers.YourApprentices.AtEnd,
+                            NewThisPeriod = report.Answers.YourApprentices.NewThisPeriod
+                        },
+                        FullTimeEquivalents = report.Answers.FullTimeEquivalents,
+                        OutlineActions = report.Answers.OutlineActions,
+                        Challenges = report.Answers.Challenges,
+                        TargetPlans = report.Answers.TargetPlans,
+                        AnythingElse = report.Answers.AnythingElse
+                    }
+                });
         }
     }
 }
