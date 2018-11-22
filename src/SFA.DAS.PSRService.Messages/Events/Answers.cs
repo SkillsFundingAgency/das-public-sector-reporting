3eb47ac55dfa8f1ec71011f4978ad96f928ac433
Neil Winstanley <Winsto@users.noreply.github.com>
2018-11-22 17:27:50 +0000

    Add message types needed for publishing.

---
+++ src/SFA.DAS.PSRService.Messages/Events/Answers.cs
@@ -0,0 +1,14 @@
+﻿namespace SFA.DAS.PSRService.Messages.Events
+{
+    public class Answers
+    {
+        public string  OrganisationName { get; set; }
+        public YourEmployees YourEmployees { get; set; }
+        public YourApprentices YourApprentices { get; set; }
+        public string FullTimeEquivalents { get; set; }
+        public string OutlineActions { get; set; }
+        public string Challenges { get; set; }
+        public string TargetPlans { get; set; }
+        public string AnythingElse { get; set; }
+    }
+}
---
+++ src/SFA.DAS.PSRService.Messages/Events/ReportSubmitted.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SFA.DAS.NServiceBus;
+
+namespace SFA.DAS.PSRService.Messages.Events
+{
+    public class ReportSubmitted
+    :Event
+    {
+        public Guid Id { get; set; }
+        public string EmployerId { get; set; }
+        public string ReportingPeriod { get; set; }
+        public Submitter Submitter { get; set; }
+        public Answers Answers { get; set; }
+        public ReportingPercentages ReportingPercentages { get; set; }
+    }
+}
