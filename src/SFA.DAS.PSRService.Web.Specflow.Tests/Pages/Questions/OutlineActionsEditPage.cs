aaed16a27ab2a44810745ef08862ba04a98b9ccc
Ashley-Bass <ashley.bass@education.gov.uk>
2019-03-19 12:57:21 +0000

    CON-289 Finalised Questions

--- src/SFA.DAS.PSRService.Web.Specflow.Tests/Pages/Questions/OutlineActionsEditPage.cs
+++ src/SFA.DAS.PSRService.Web.Specflow.Tests/Pages/Questions/OutlineActionsEditPage.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.PSRService.Web.Specflow.Tests.Pages.Questions
 {
     public class OutlineActionsEditPage : FactorsQuestionEditPage
     {
-        protected override string PAGE_TITLE => "What actions have you taken this tax year to meet the target, and how do these compare to the challenges experienced in 17/18?";
+        protected override string PAGE_TITLE => "What actions have you taken this year to meet the target? How do these compare to the challenges experienced in the previous year?";
 
         protected override string PageUrl => QuestionPageUrls.OutlineActions;
 
