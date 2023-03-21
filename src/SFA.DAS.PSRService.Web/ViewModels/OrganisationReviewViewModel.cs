5ead2301e5df12213259c89e22be7d47052bad89
ratheeshri1984 <ratheeshri1984@gmail.com>
2023-03-21 16:51:02 +0000

    MAP-201-ViewDataDictionary changed to view model

---
+++ src/SFA.DAS.PSRService.Web/ViewModels/OrganisationReviewViewModel.cs
@@ -0,0 +1,12 @@
+﻿using StructureMap.Query;
+
+namespace SFA.DAS.PSRService.Web.ViewModels
+{
+    public class OrganisationReviewViewModel
+    {
+        public string OrganisationName { get; set; }
+        public bool? HasMinimumEmployeeHeadcount { get; set; }
+        public bool? IsLocalAuthority { get; set; }
+
+    }
+}
