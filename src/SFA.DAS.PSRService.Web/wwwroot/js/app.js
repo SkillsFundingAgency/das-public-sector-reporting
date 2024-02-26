a8e757e3fbb75043249745ad1f2e5b5ff9e71fee
James King <hello@jamesmichaelking.com>
2024-02-26 16:25:55 +0000

    Update layout and employer shared ui

--- src/SFA.DAS.PSRService.Web/wwwroot/js/app.js
+++ src/SFA.DAS.PSRService.Web/wwwroot/js/app.js
@@ -1,52 +1,47 @@
-
-
-window.GOVUKFrontend.initAll();
 window.DASFrontend.initAll();
 
+(function () {
+  "use strict";
+  var root = this;
 
+  if (typeof root.GOVUK === "undefined") {
+    root.GOVUK = {};
+  }
 
-(function() {
-    'use strict';
-    var root = this;
-  
-    if (typeof root.GOVUK === 'undefined') {
-      root.GOVUK = {};
-    }
-  
-    GOVUK.addCommas = {
-      elems: $('.js-addcommas'),
-      init: function() {
-        if (!this.elems.length) return false;
-        $(window).on('load', this.onLoaded(this));
-        this.elems.on('input', this.onInput(this));
-      },
-      onInput: function(self) {
-        return function(event) {
-          event.target.value = self.convertString(event.target.value);
-        };
-      },
-      onLoaded: function(self) {
-        return function() {
-          self.elems.each(function() {
-            if ($(this).is('input')) {
-              $(this).val(self.convertString($(this).val()));
-            } else {
-              $(this).text(self.convertString($(this).text()));
-            }
-          });
-        };
-      },
-      convertString: function(string) {
-        return string.match(/^([0])\1*$/g)
-          ? '0'
-          : string
-              .replace(/\D/g, '') // removes all chars that are not digits
-              .replace(/^0+/g, '') // remove leading zeroes
-              .replace(/\B(?=(\d{3})+(?!\d))/g, ','); // add commas every 3 digits
-      }
-    };
-  
-    if (window.GOVUK && GOVUK.addCommas) {
-      GOVUK.addCommas.init();
-    }
-  }.call(this));
+  GOVUK.addCommas = {
+    elems: $(".js-addcommas"),
+    init: function () {
+      if (!this.elems.length) return false;
+      $(window).on("load", this.onLoaded(this));
+      this.elems.on("input", this.onInput(this));
+    },
+    onInput: function (self) {
+      return function (event) {
+        event.target.value = self.convertString(event.target.value);
+      };
+    },
+    onLoaded: function (self) {
+      return function () {
+        self.elems.each(function () {
+          if ($(this).is("input")) {
+            $(this).val(self.convertString($(this).val()));
+          } else {
+            $(this).text(self.convertString($(this).text()));
+          }
+        });
+      };
+    },
+    convertString: function (string) {
+      return string.match(/^([0])\1*$/g)
+        ? "0"
+        : string
+            .replace(/\D/g, "") // removes all chars that are not digits
+            .replace(/^0+/g, "") // remove leading zeroes
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ","); // add commas every 3 digits
+    },
+  };
+
+  if (window.GOVUK && GOVUK.addCommas) {
+    GOVUK.addCommas.init();
+  }
+}).call(this);
