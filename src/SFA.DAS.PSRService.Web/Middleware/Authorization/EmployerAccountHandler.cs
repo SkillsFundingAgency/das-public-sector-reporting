f22328937172aff3a458fa4b9be8d293650b59b8
Jon Harris <jon.harris@fuitsu.com>
2024-09-17 10:38:07 +0100

    Fixing warnings and refactor

--- src/SFA.DAS.PSRService.Web/Middleware/Authorization/EmployerAccountHandler.cs
+++ src/SFA.DAS.PSRService.Web/Middleware/Authorization/EmployerAccountHandler.cs
@@ -9,40 +9,39 @@ using SFA.DAS.PSRService.Web.Configuration;
 using SFA.DAS.PSRService.Web.Configuration.Authorization;
 using SFA.DAS.PSRService.Web.Models;
 
-namespace SFA.DAS.PSRService.Web.Middleware.Authorization
+namespace SFA.DAS.PSRService.Web.Middleware.Authorization;
+
+public class EmployerAccountHandler(IHttpContextAccessor httpContextAccessor) : AuthorizationHandler<EmployerAccountRequirement>
 {
-    public class EmployerAccountHandler : AuthorizationHandler<EmployerAccountRequirement>
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, EmployerAccountRequirement requirement)
     {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-
-        public EmployerAccountHandler(IHttpContextAccessor httpContextAccessor)
+        if (!httpContextAccessor.HttpContext.Request.RouteValues.ContainsKey(RouteValues.HashedEmployerAccountId))
         {
-            _httpContextAccessor = httpContextAccessor;
+            return Task.CompletedTask;
         }
-        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, EmployerAccountRequirement requirement)
-        {
-            if (!_httpContextAccessor.HttpContext.Request.RouteValues.ContainsKey(RouteValues.HashedEmployerAccountId))
-            {
-                return;
-            }
-            
-            var accountIdFromUrl = _httpContextAccessor.HttpContext.Request.RouteValues[RouteValues.HashedEmployerAccountId].ToString().ToUpper();
-            
-            if (context.User.HasClaim(c => c.Type.Equals(EmployerPsrsClaims.AccountsClaimsTypeIdentifier)))
-            {
-                var employerAccountClaim = context.User.FindFirst(c => c.Type.Equals(EmployerPsrsClaims.AccountsClaimsTypeIdentifier));
-                var employerAccounts = JsonConvert.DeserializeObject<Dictionary<string, EmployerIdentifier>>(employerAccountClaim?.Value);
 
-                if (employerAccountClaim != null && employerAccounts.ContainsKey(accountIdFromUrl))
-                {
-                    if (context.Resource is AuthorizationFilterContext mvcContext && !mvcContext.HttpContext.Items.ContainsKey(ContextItemKeys.EmployerIdentifier))
-                    {
-                        mvcContext.HttpContext.Items.Add(ContextItemKeys.EmployerIdentifier, employerAccounts.GetValueOrDefault(accountIdFromUrl));
-                    }
-                    
-                    context.Succeed(requirement);
-                }
-            }
+        var accountIdFromUrl = httpContextAccessor.HttpContext.Request.RouteValues[RouteValues.HashedEmployerAccountId].ToString().ToUpper();
+
+        if (!context.User.HasClaim(c => c.Type.Equals(EmployerPsrsClaims.AccountsClaimsTypeIdentifier)))
+        {
+            return Task.CompletedTask;
         }
+
+        var employerAccountClaim = context.User.FindFirst(c => c.Type.Equals(EmployerPsrsClaims.AccountsClaimsTypeIdentifier));
+        var employerAccounts = JsonConvert.DeserializeObject<Dictionary<string, EmployerIdentifier>>(employerAccountClaim?.Value);
+
+        if (employerAccountClaim == null || !employerAccounts.ContainsKey(accountIdFromUrl))
+        {
+            return Task.CompletedTask;
+        }
+            
+        if (context.Resource is AuthorizationFilterContext mvcContext && !mvcContext.HttpContext.Items.ContainsKey(ContextItemKeys.EmployerIdentifier))
+        {
+            mvcContext.HttpContext.Items.Add(ContextItemKeys.EmployerIdentifier, employerAccounts.GetValueOrDefault(accountIdFromUrl));
+        }
+
+        context.Succeed(requirement);
+
+        return Task.CompletedTask;
     }
 }
