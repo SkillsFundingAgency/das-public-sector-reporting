ab2ba199e3f8256912601a4d4dd8118d1bbdc657
Jon Harris <jon.harris@fuitsu.com>
2024-09-17 14:37:30 +0100

    Tidying

--- src/SFA.DAS.PSRService.Messages/Events/ReportingPercentages.cs
+++ src/SFA.DAS.PSRService.Messages/Events/ReportingPercentages.cs
@@ -1,15 +1,14 @@
-﻿namespace SFA.DAS.PSRService.Messages.Events
+﻿namespace SFA.DAS.PSRService.Messages.Events;
+
+public class ReportingPercentages 
 {
-    public class ReportingPercentages 
+    public ReportingPercentages()
     {
-        public ReportingPercentages()
-        {
-            EmploymentStarts = "0.00";
-            TotalHeadCount = "0.00";
-            NewThisPeriod = "0.00";
-        }
-        public string EmploymentStarts { get; set; }
-        public string TotalHeadCount { get; set; }
-        public string NewThisPeriod { get; set; }
+        EmploymentStarts = "0.00";
+        TotalHeadCount = "0.00";
+        NewThisPeriod = "0.00";
     }
+    public string EmploymentStarts { get; set; }
+    public string TotalHeadCount { get; set; }
+    public string NewThisPeriod { get; set; }
 }
